c2b5049c88003fe1e0636cddc07cc8b33871225b
huihuangyue321x <1521566823@qq.com>
2022-08-10 17:22:37 +0800

    first

--- leetcode11.java
+++ leetcode11.java
@@ -1,5 +1,19 @@
+import javax.imageio.ImageTranscoder;
+
 public class leetcode11 {
     public static void main(String[] args) {
-        System.out.println("world");
+        int []height={1,8,6,2,5,4,8,3,7};
+        int i,j;
+        i=0;
+        j=height.length-1;
+        int ans=0;
+        while(i!=j){
+            int area = Math.min(height[i], height[j]) * (j - i);
+            ans=Math.max(area,ans);
+            if(height[i]<=height[j])i++;
+            else j--;
+        }
+
+        System.out.println(ans);
     }
 }
